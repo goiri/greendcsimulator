6e688f6bee9f306a2958fae60dfe6eea98669aae
goiri <elgoiri@gmail.com>
2013-01-18 13:09:29 -0500

    Added script.

---
+++ simulator.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export GUROBI_HOME=/home/goiri/hadoop-parasol/solver/gurobi500/linux64/
+export GRB_LICENSE_FILE=$GUROBI_HOME/gurobi-`hostname`.lic
+export LD_LIBRARY_PATH=$GUROBI_HOME/lib
+
+python2.7 simulator.py
