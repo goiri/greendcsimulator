0fd65ea0a9943d4c95538a41918d7de76095573c
Inigo Goiri <elgoiri@gmail.com>
2013-01-22 05:31:03 -0500

    Fix for remote running.

--- simulator.sh
+++ simulator.sh
@@ -2,7 +2,13 @@
 
 # export GUROBI_HOME=/home/goiri/hadoop-parasol/solver/gurobi500/linux64/
 export GUROBI_HOME=/home/goiri/gurobi510/linux64/
-export GRB_LICENSE_FILE=$GUROBI_HOME/gurobi-`hostname`.lic
 export LD_LIBRARY_PATH=$GUROBI_HOME/lib
+# Check licence
+if [ -e $GUROBI_HOME/gurobi-`hostname`.lic ]; then
+	export GRB_LICENSE_FILE=$GUROBI_HOME/gurobi-`hostname`.lic
+fi
+if [ -e $GUROBI_HOME/gurobi.lic.`hostname` ]; then
+	export GRB_LICENSE_FILE=$GUROBI_HOME/gurobi.lic.`hostname`
+fi
 
 python2.7 simulator.py $*
