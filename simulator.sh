51dd19a025198074666ad76876558c8f17bdeff9
Inigo Goiri <elgoiri@gmail.com>
2013-01-22 05:28:34 -0500

    Fix for remote running.

--- simulator.sh
+++ simulator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # export GUROBI_HOME=/home/goiri/hadoop-parasol/solver/gurobi500/linux64/
-export GUROBI_HOME=/home/goirix/gurobi510/linux64/
+export GUROBI_HOME=/home/goiri/gurobi510/linux64/
 export GRB_LICENSE_FILE=$GUROBI_HOME/gurobi-`hostname`.lic
 export LD_LIBRARY_PATH=$GUROBI_HOME/lib
 
