91c4c5e1febf732baba87d7aa9060d119f00b2c9
goiri <elgoiri@gmail.com>
2013-02-20 22:37:14 -0500

    Clean experimenter

--- experimenter.sh
+++ experimenter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Experiment duration
 PERIOD="1y"
 
 # Independent workloads
@@ -9,48 +10,51 @@ WORKLOAD="data/workload/messenger.workload"
 WORKLOAD="data/workload/wikipedia.workload"
 WORKLOAD="data/workload/flash.workload"
 WORKLOAD="data/workload/variable.workload"
-# Workloads
-WORKLOADS="data/workload/asplos.workload data/workload/variable.workload data/workload/hotmail.workload data/workload/messenger.workload data/workload/wikipedia.workload data/workload/flash.workload data/workload/search.workload data/workload/orkut.workload data/workload/mix.workload"
-WORKLOADS="data/workload/mix.workload"
+WORKLOAD="data/workload/mix.workload"
+# All workloads
+WORKLOADS="data/workload/asplos.workload data/workload/hotmail.workload data/workload/messenger.workload data/workload/wikipedia.workload data/workload/flash.workload data/workload/search.workload data/workload/orkut.workload data/workload/mix.workload"
 
 # Locations
-LOCATIONS="data/newark.location data/quito.location"
+LOCATIONS=""
+LOCATIONS=$LOCATIONS"data/locations/newark.location"
+LOCATIONS=$LOCATIONS"data/locations/quito.location"
+# Net metering. It's already defined in the location
+NETMETER_0=0.0  #   0%
+NETMETER_WR=0.4 #  40%
+NETMETER_RR=1.0 # 100%
 
-NETMETER_0=0.0
-NETMETER_WR=0.4
-NETMETER_RR=1.0
-
-# Threading
+# Multiprocess
 MAXTHREADS=`cat /proc/cpuinfo  | grep processor | wc -l`
-# MAXTHREADS=4
 NUMTHREADS=0
 
 # Parse solar modes
-SOLARMODES="3200 0 800 1600 2400"
+SOLARS="3200 0 800 1600 2400"
 if [ $# -ge 1 ]; then
-	SOLARMODES=$*
+	SOLARS=$*
 fi
 
 if true; then
 	for ALWAYSON in "" "--alwayson"; do
-		for WORKLOAD in $WORKLOADS; do
-			for DELAY in "--delay" ""; do
-				for SOLAR in $SOLARMODES; do
-					for BATTERY in 0 32000 8000 16000 24000; do
-						# Wait for empty slots
-						if [ $NUMTHREADS -ge $MAXTHREADS ]; then
-							# We have filled all the threads, wait for them to finish
-							echo "`date`: Waiting for ${THREADS[@]}"
-							for THREADID in ${THREADS[@]}; do
-								wait $THREADID
-							done
-							NUMTHREADS=0
-						fi
-						# Run more simulations
-						bash simulator.sh --solar $SOLAR --battery $BATTERY --period $PERIOD --workload $WORKLOAD --net $NETMETER_WR $DELAY $ALWAYSON > /dev/null &
-						# Store the PID
-						THREADS[$NUMTHREADS]=$!
-						let NUMTHREADS=$NUMTHREADS+1
+		for LOCATION in $LOCATIONS; do
+			for WORKLOAD in $WORKLOADS; do
+				for DELAY in "--delay" ""; do
+					for SOLAR in $SOLARS; do
+						for BATTERY in 0 32000 8000 16000 24000; do
+							# Wait for empty slots
+							if [ $NUMTHREADS -ge $MAXTHREADS ]; then
+								# We have filled all the threads, wait for them to finish
+								echo "`date`: Waiting for ${THREADS[@]}"
+								for THREADID in ${THREADS[@]}; do
+									wait $THREADID
+								done
+								NUMTHREADS=0
+							fi
+							# Run more simulations
+							bash simulator.sh --solar $SOLAR --battery $BATTERY --period $PERIOD --workload $WORKLOAD -l $LOCATION $DELAY $ALWAYSON > /dev/null &
+							# Store the PID
+							THREADS[$NUMTHREADS]=$!
+							let NUMTHREADS=$NUMTHREADS+1
+						done
 					done
 				done
 			done
@@ -111,5 +115,3 @@ if false; then
 
 	echo "Experiment finishes at: "`date`
 fi
-
-
